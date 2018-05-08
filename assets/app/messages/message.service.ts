e26394be822cac9696c03ddf8772027f6e92b435
debjohnson <debjohnson_11@yahoo.com>
2018-05-08 17:06:05 -0500

    Added http module/post to addMessage function

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,11 +1,18 @@
+import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
+
 import { Message } from "./message.model";
 
+@Injectable()
 export class MessageService {
 	private messages: Message[] = [];
 
+	constructor(private http: Http) {}
+
 	addMessage(message: Message) {
 		this.messages.push(message);
-		console.log(this.messages);
+		const body = JSON.stringify(message);
+		this.http.post('http://localhost:3000/message', body);
 	}	
 
 	getMessages() {
