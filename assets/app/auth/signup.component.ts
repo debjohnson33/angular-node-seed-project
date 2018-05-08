a3ad52fef07392a004502dfc86551ea05f3990e0
debjohnson <debjohnson_11@yahoo.com>
2018-05-08 16:35:14 -0500

    Edited signin/signup forms/components & turned on delete functionality

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -14,12 +14,15 @@ export class SignupComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.myForm = new FormGroup({
-			firstName: new FormControl(null, Validators.required),
-			lastName: new FormControl(null, Validators.required),
-			email: new FormControl(null, Validators.required),
-			password: new FormControl(null, Validators.required)
-		});
-	}
+        this.myForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 
 }
