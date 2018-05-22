d42e77c8e2b650016899224489e820d244da92ee
debjohnson <debjohnson_11@yahoo.com>
2018-05-22 16:40:24 -0500

    Connected user to message

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 
+var User = require('../models/user');
 var Message = require('../models/message');
 
 router.get('/', function(req, res, next){
